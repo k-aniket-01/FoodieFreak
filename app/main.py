5b58354bb68c4f19c0b5849e199af2f873f8a8ad
Aniket Khomane <khomaneaniket9420@gmail.com>
2026-06-29 16:47:45 +0530

    [feat] users orders api

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI 
 from app.routers import authentication, users
+from test import test_router
 
 app = FastAPI()
 #uvicorn app.main:app --reload
 
-app.include_router(authentication.router)
-app.include_router(users.router)
+app.include_router(authentication.router, tags=["Authentication"])
+app.include_router(users.router,tags=['Users Router'])
+app.include_router(test_router, tags=['test routers'])
--- app/routers/users.py
+++ app/routers/users.py
@@ -17,13 +17,10 @@ def update_user(
     user = Depends(get_current_user),
     db : Session = Depends(get_db) 
 ):
-    
     values = dict(data)
-    
     db.query(User).filter(User.id == user.id).update(values)
     db.commit()
     db.refresh(user)
-
     return user 
 
 @router.delete('/delete/user')
@@ -34,5 +31,23 @@ def delete_user(
     db.query(User).filter(User.id==user.id).update({"is_deleted":True})
     db.commit()
     db.refresh(user)
-   
     return user
+
+@router.get('/user/orders/')
+def get_user_orders(db:Session=Depends(get_db), user:User= Depends(get_current_user)):
+    query = (
+        db.query(User, Order)
+        .join(Order, User.id == Order.user_id)
+        .filter(User.id == user.id)
+        .all()
+             )
+    data = [
+        {
+            "name":user.name, 
+            "order":order.id,
+            "amount":order.total_amount,
+            "status":order.status
+            }
+            for user, order in query
+            ]
+    return data
