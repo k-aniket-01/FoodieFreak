e0c6254f7e206e71efdec4cf482ed2804e168f17
Aniket Khomane <khomaneaniket9420@gmail.com>
2026-08-06 14:08:18 +0530

    [fix] typo

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from app.daily_menu.router.daily_menu_router import daily_menu_router
 from app.carts.router.carts_router import carts_router
 from app.orders.router.orders_router import orders_router
 from app.payments.router.payments_router import payment_router
-from app.notifications.router.notification_router import notfication_router
+from app.notifications.router.notification_router import notification_router
 # from test import test_router
 
 
@@ -54,5 +54,5 @@ app.include_router(daily_menu_router, tags=["Daily Menu Routers"])
 app.include_router(carts_router, tags=["Cart Routers"])
 app.include_router(orders_router, tags=["Order Routers"])
 app.include_router(payment_router, tags=["Payment Routers"])
-app.include_router(notfication_router, tags=["Notification Routers"])
+app.include_router(notification_router, tags=["Notification Routers"])
 # app.include_router(test_router, tags=['test routers'])
